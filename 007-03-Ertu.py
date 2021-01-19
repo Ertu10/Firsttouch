1b418946c010cfa8e16b5c559a246a563e74d898
Ertuğrul <76111155+Ertu10@users.noreply.github.com>
2021-01-19 16:36:11 +0300

    Assignment 007/03 homework

---
+++ 007-03-Ertu.py
@@ -0,0 +1,18 @@
+#C7114-Ertu
+num = input("bir sayı giriniz: ")
+
+count = ii = 0
+if  num.isnumeric():
+    for i in num:
+      power = len(num)
+    
+      count = count + (int(num[ii])**power)
+      ii += 1
+    if count==int(num):
+        print("BU sayı Armstrong sayıdır.")
+    else:
+       print("malesef bu sayı armstrong değil :(")
+else:
+  print("Adam akıllı sayı gir.")
+
+#Direk çalıştır hazır kurulum.
