96c9105030ff434fcaa02ebb656c845b4fe1198f
Ertuğrul <76111155+Ertu10@users.noreply.github.com>
2021-01-21 22:42:08 +0300

    007-01

---
+++ 007-01-Ertu.py
@@ -0,0 +1,7 @@
+#C7114-Ertu
+user_name = input("Please, write your name = ")
+password = "19671453"
+if user_name == "Ertu":
+  print(f"Hello {user_name} this is your password '{password}'.")
+else:
+  print(f"Hi {user_name}. See you later..")
